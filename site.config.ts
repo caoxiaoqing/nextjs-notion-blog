0088ad42e43520eccf9aef4b23d2dd760d5fa45e
zhangqiyuan <CaoXiaoqing2008@gmail.com>
2022-11-20 20:48:23 +0800

    Update site.config.ts
    change to demo page

--- site.config.ts
+++ site.config.ts
@@ -2,7 +2,8 @@ import { siteConfig } from './lib/site-config'
 
 export default siteConfig({
   // the site's root Notion page (required)
-  rootNotionPageId: 'Solaris-c13c1a49716647f98f089db05b7686c2',
+  // rootNotionPageId: 'Solaris-c13c1a49716647f98f089db05b7686c2',
+  rootNotionPageId: 'solaris-s-bloooooog-05937a16c169417d97aaefc56565c055',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
